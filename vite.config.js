7163b76af97bd2c312b79d13e7e5cdd08b91370f
abdulazizalharbi <azizbaristaa@gmail.com>
2025-10-08 23:28:34 +0300

    Updated vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
+    base: '/',
     server: {
         port: 3000,
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
     },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
   },
 })
