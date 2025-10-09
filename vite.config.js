d423ee4051902b6a5e55babab8b4c22de1203d21
abdulazizalharbi <azizbaristaa@gmail.com>
2025-10-10 01:15:10 +0300

    Added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: '/AbdulazizAlharbi/',
+    base: '/',
     server: {
         port: 3000,
     },
